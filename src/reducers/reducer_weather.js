e8a7884fba51f70cadd007f05182e55982881ab0
BiggaHD <dzenis.hankusic@gmail.com>
2019-02-26 00:20:57 +0100

    Small improvments

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -7,6 +7,6 @@ export default function (state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
   return [action.payload.data, ...state];
+  default: return state;
   }
-  return state;
 }
